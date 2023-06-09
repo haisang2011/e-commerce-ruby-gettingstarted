2bc600574d9e684846594f6b88758de04cb726e8
Khanhhh <khongduykhanh2000@gmail.com>
2023-06-09 15:50:05 +0700

    Create tables: products, orders, order_details

---
+++ db/migrate/20230609083211_create_order_details.rb
@@ -0,0 +1,12 @@
+class CreateOrderDetails < ActiveRecord::Migration[7.0]
+  def change
+    create_table :order_details do |t|
+      t.bigint "order_id"
+      t.bigint "product_id"
+      t.integer "product_price"
+      t.integer "quantity", default: 1
+    end
+    add_foreign_key :order_details, :orders, column: :order_id, primary_key: :id
+    add_foreign_key :order_details, :products, column: :product_id, primary_key: :id
+  end
+end
